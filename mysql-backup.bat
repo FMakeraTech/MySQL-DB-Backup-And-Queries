de6a2a1fafda168e988d40a800f6397c7552650e
FMakeraTech <fe.makera@gmail.com>
2024-08-01 03:16:24 -0400

    Importation des bases de données et suppression des fichiers SQL

--- mysql-backup.bat
+++ mysql-backup.bat
@@ -26,3 +26,16 @@ REM Compression of the DB dump files into CAB files
 MAKECAB "ocadmin_dbo.%TIMESTAMP%.sql" "ocadmin_dbo.%TIMESTAMP%.sql.cab"
 MAKECAB "openclinic_dbo.%TIMESTAMP%.sql" "openclinic_dbo.%TIMESTAMP%.sql.cab"
 MAKECAB "ocstats_dbo.%TIMESTAMP%.sql" "ocstats_dbo.%TIMESTAMP%.sql.cab"
+
+REM Import the 3 databases in the windows MySQL DB 
+REM the windows  credentials for MySQL are:
+REM user: root
+REM password: openclinic
+ "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -u root -popenclinic ocadmin_dbo < "ocadmin_dbo.%TIMESTAMP%.sql" 
+ "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -u root -popenclinic openclinic_dbo < "openclinic_dbo.%TIMESTAMP%.sql"
+ "C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" -u root -popenclinic ocstats_dbo < "ocstats_dbo.%TIMESTAMP%.sql"
+
+REM Delete uncompressed DB dump files to free space.
+ DEL /q /f "ocadmin_dbo.%TIMESTAMP%.sql"
+ DEL /q /f "openclinic_dbo.%TIMESTAMP%.sql"
+ DEL /q /f "ocstats_dbo.%TIMESTAMP%.sql"
